07897d82f9288f12f13bff35ed421e439d785b01
Victor Bezerra <vctr.mb@gmail.com>
2023-04-22 17:02:39 -0300

    funnel step 5 done

--- templates/create-account-step-four.tsx
+++ templates/create-account-step-four.tsx
@@ -7,13 +7,40 @@ import { OptionContent } from './option-content'
 import { Checkbox } from '@/components/checkbox'
 
 const validationSchema = z.object({
-  cabernetSauvignon: z.boolean(),
+  favoriteTypeWine: z.array(z.string()).nonempty(),
 })
 
 type FormFields = z.infer<typeof validationSchema>
 type Props = {
   onNext: (data: FormFields) => void
 }
+
+const options = [
+  {
+    label: 'Cabernet Sauvignon',
+    value: 'cabernetSauvignon',
+  },
+  {
+    label: 'Merlot',
+    value: 'merlot',
+  },
+  {
+    label: 'Chardonnay',
+    value: 'chardonnay',
+  },
+  {
+    label: 'Sauvignon Blanc',
+    value: 'sauvignonBlanc',
+  },
+  {
+    label: 'Pinot Grigio',
+    value: 'pinotGrigio',
+  },
+  {
+    label: 'Pinot Noir',
+    value: 'pinotNoir',
+  },
+]
 
 export function CreateAccountStepFour({ onNext }: Props) {
   const stepper = useStepperContext()
@@ -24,6 +51,9 @@ export function CreateAccountStepFour({ onNext }: Props) {
   } = useForm<FormFields>({
     resolver: zodResolver(validationSchema),
     mode: 'onChange',
+    defaultValues: {
+      favoriteTypeWine: [],
+    },
   })
 
   const onSubmit = handleSubmit((data) => {
@@ -41,70 +71,36 @@ export function CreateAccountStepFour({ onNext }: Props) {
           <div className="grid grid-cols-1 lg:grid-cols-[repeat(2,_minmax(16rem,_1fr))] gap-2">
             <Controller
               control={control}
-              name="cabernetSauvignon"
-              render={({ field: { onChange, value, ref } }) => {
+              name="favoriteTypeWine"
+              render={({ field: { onChange, value } }) => {
                 return (
-                  <OptionContent>
-                    <Checkbox.Root
-                      id="c1"
-                      ref={ref}
-                      name="cabernetSauvignon"
-                      onCheckedChange={onChange}
-                      checked={value}
-                      aria-label="Cabernet Sauvignon"
-                    >
-                      <Checkbox.Indicator />
-                    </Checkbox.Root>
-                    <Checkbox.Label htmlFor="c1">
-                      Cabernet Sauvignon
-                    </Checkbox.Label>
-                  </OptionContent>
-                )
-              }}
-            />
-            <Controller
-              control={control}
-              name="cabernetSauvignon"
-              render={({ field: { onChange, value, ref } }) => {
-                return (
-                  <OptionContent>
-                    <Checkbox.Root
-                      id="c1"
-                      ref={ref}
-                      name="cabernetSauvignon"
-                      onCheckedChange={onChange}
-                      checked={value}
-                      aria-label="Cabernet Sauvignon"
-                    >
-                      <Checkbox.Indicator />
-                    </Checkbox.Root>
-                    <Checkbox.Label htmlFor="c1">
-                      Cabernet Sauvignon
-                    </Checkbox.Label>
-                  </OptionContent>
-                )
-              }}
-            />
-            <Controller
-              control={control}
-              name="cabernetSauvignon"
-              render={({ field: { onChange, value, ref } }) => {
-                return (
-                  <OptionContent>
-                    <Checkbox.Root
-                      id="c1"
-                      ref={ref}
-                      name="cabernetSauvignon"
-                      onCheckedChange={onChange}
-                      checked={value}
-                      aria-label="Cabernet Sauvignon"
-                    >
-                      <Checkbox.Indicator />
-                    </Checkbox.Root>
-                    <Checkbox.Label htmlFor="c1">
-                      Cabernet Sauvignon
-                    </Checkbox.Label>
-                  </OptionContent>
+                  <>
+                    {options.map((option) => (
+                      <OptionContent key={option.value}>
+                        <Checkbox.Root
+                          aria-label={option.label}
+                          id={`c-${option.value}`}
+                          checked={value?.includes(option.value)}
+                          onCheckedChange={(checked) => {
+                            if (checked) {
+                              onChange([...value, option.value])
+                            } else {
+                              onChange(
+                                value.filter(
+                                  (oldVal) => oldVal !== option.value
+                                )
+                              )
+                            }
+                          }}
+                        >
+                          <Checkbox.Indicator />
+                        </Checkbox.Root>
+                        <Checkbox.Label htmlFor={`c-${option.value}`}>
+                          {option.label}
+                        </Checkbox.Label>
+                      </OptionContent>
+                    ))}
+                  </>
                 )
               }}
             />
